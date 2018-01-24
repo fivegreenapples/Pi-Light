fd8d835c1550852a892f9ab3282738db7e9c71ba
Ben Holland <github@fivegreenapples.com>
2018-01-24 14:54:13 +0000

    Fix ui path

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ func main() {
 	mux.Handle(pat.Get("/ws/0/*"), universes[0].mux())
 	mux.Handle(pat.Get("/ws/1/*"), universes[1].mux())
 
-	http.Handle("/ui", http.FileServer(http.Dir("/home/pi/go/src/github.com/fivegreenapples/PiLight/ui")))
+	http.Handle("/ui", http.FileServer(http.Dir("/home/pi/go/src/github.com/fivegreenapples/Pi-Light/ui")))
 	http.Handle("/ws", mux)
 
 	log.Println("Starting pilight webservice")
